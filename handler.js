6045b455c681887372d32244cce3aa9ddbe4154e
msevillano <sevillano.mauro@gmail.com>
2018-07-30 16:10:30 -0300

    Mother of refactor!

--- handler.js
+++ handler.js
@@ -2,116 +2,101 @@ const request = require('request');
 
 const SERVER_IP = process.env.SERVERIP;
 
-module.exports.onStart = () => {
-    let response = 0;
+module.exports.changeMovies = async () => {
+    try {
+        let movies = JSON.parse(await getMovies(SERVER_IP + '/movies'));
+        movies.forEach(movie => {
+            deleteMovie(SERVER_IP + '/movies/', movie.title);
+        });
+        for (let i = 0; i < 10; i++) {
+            addRandomMovie(SERVER_IP + '/movies/', generateRandomMovie());
+        }
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+module.exports.increaseViewers = async () => {
+    try {
+        let movies = JSON.parse(await getMovies(SERVER_IP + '/movies'));
+        let movie = movies[Math.floor((Math.random() * 10))];
+        movie.viewers ++;
+        updateMovie(SERVER_IP + '/movies/', movie);
+
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+module.exports.generateStats = async () => {
 
     try {
-        request(SERVER_IP + '/movies', (err, res, body) => {
-            if (err) throw err;
-            if (res.status !== 200) response = 1;
-            let movies = JSON.parse(body);
-            movies.forEach(movie => {
-                request({url: SERVER_IP + '/movies/'+ movie.title,
-                    method: 'DELETE'
-                }, (err, res) => {
-                    if(err) throw err;
-                    if (res.status !== 200) response = 1;
-                });
-            });
+        let movies = JSON.parse(await getMovies(SERVER_IP + '/movies'));
+        movies.sort((a, b) => {return b.viewers - a.viewers});
+        while (movies.length > 5) {
+            movies.pop();
+        }
+        return movies;
+    } catch (err) {
+        console.error(err);
+    }
+};
 
-            for (let i = 0; i < 10; i++) {
-                let newMovie = generateRandomMovie();
-                request({url: SERVER_IP + '/movies/',
-                    method: 'POST',
-                    json: newMovie
-                }, (err, res) => {
-                    if(err) throw err;
-                    if (res.status !== 201) response = 1;
-                });
+function getMovies (url) {
+    return new Promise((resolve, reject) => {
+        request(url, (err, res, body) => {
+            if (!err && res.statusCode === 200) {
+                resolve(body);
+            } else {
+                reject(err);
             }
         });
-        return response;
-    } catch (err) {
-        console.log(err);
-        return 1;
-    }
-};
+    });
+}
 
-module.exports.increaseViewers = () => {
-    let response = 0;
-    try {
-        request(SERVER_IP + '/movies', (err, res, body) => {
-            if (err) throw err;
-            if (res.status !== 200) response = 1;
-            let movies = JSON.parse(body);
-            let movie = movies[Math.floor((Math.random() * 10))];
-            movie.viewers ++;
-            request({url: SERVER_IP + '/movies/'+ movie.title,
-                     method: 'PUT',
-                     json: movie
-                    }, (err, res) => {
-                if(err) throw err;
-                if (res.status !== 200) response = 1;
-            });
-        });
-        return response;
-    } catch (err) {
-        console.log(err);
-        return 1;
-    }
-};
-
-module.exports.generateStats = () => {
-    let response = 0;
-    try {
-
-        request(SERVER_IP + '/movies', (err, res, body) => {
-            if (err) throw err;
-            if (res.status !== 200) response = 1;
-            let movies = JSON.parse(body);
-            movies.sort((a, b) => {return b.viewers - a.viewers});
-            while (movies.length > 5) {
-                movies.pop();
+function addRandomMovie (url, newMovie) {
+    return new Promise((resolve, reject) => {
+        request({url: url,
+            method: 'POST',
+            json: newMovie
+        },(err, res) =>  {
+            if (!err && res.statusCode === 201) {
+                resolve();
+            } else {
+                reject(err);
             }
-           response = movies;
         });
-        return response;
-    } catch (err) {
-        console.log(err);
-        return 1;
-    }
-};
+    });
+}
 
-module.exports.changeMovies = () => {
-    let response = 0;
-
-    try {
-        request(SERVER_IP + '/movies', (err, res, body) => {
-            if (err) throw err;
-            if (res.status !== 200) response = 1;
-            let movies = JSON.parse(body);
-            movies.forEach(movie => {
-                request({
-                    url: SERVER_IP + '/movies/' + movie.title,
-                    method: 'DELETE'
-                });
-                let newMovie = generateRandomMovie();
-                request({
-                    url: SERVER_IP + '/movies/',
-                    method: 'POST',
-                    json: newMovie
-                }, (err, res) => {
-                    if (err) throw err;
-                    if (res.status !== 200) response = 1;
-                });
-            });
+function updateMovie (url, newMovie) {
+    return new Promise((resolve, reject) => {
+        request({url: url + newMovie.title,
+            method: 'PUT',
+            json: newMovie
+        },(err, res) =>  {
+            if (!err && res.statusCode === 200) {
+                resolve();
+            } else {
+                reject(err);
+            }
         });
-    return response;
-    } catch (err) {
-        console.log(err);
-        return 1;
-    }
-};
+    });
+}
+
+function deleteMovie(url, title) {
+    return new Promise((resolve, reject) => {
+        request({url: url + title,
+            method: 'DELETE'
+        },(err, res) =>  {
+            if (!err && res.statusCode === 200) {
+                resolve();
+            } else {
+                reject(err);
+            }
+        });
+    });
+}
 
 function generateRandomMovie() {
     return {
--- test/handler.test.js
+++ test/handler.test.js
@@ -5,93 +5,94 @@ const chai = require('chai');
 const should = chai.should();
 const nock = require('nock');
 
-let api1 = nock(process.env.SERVERIP)
-    .get('/movies/')
+let apiGet = nock(process.env.SERVERIP).persist().get('/movies')
     .reply(200, [
-        {
-            "actors": [
-                "ywknirn",
-                "gczqzne",
-                "qjmlhysn"
-            ],
-            "_id": "5b5ceb9a929eb815f6cda063",
-            "title": "lzjck",
-            "premiere": "2018-07-28T03:00:00.000Z",
-            "director": "ygerhnwwxp",
-            "viewers": 0,
-            "__v": 0
-        }
+        {"actors": ["ywknirn", "gczqzne", "qjmlhysn"], "title": "lzjck1", "premiere": "2018-07-28T03:00:00.000Z", "director": "ygerhnwwxp", "viewers": 1},
+        {"actors": ["ywknirn", "gczqzne", "qjmlhysn"], "title": "lzjck2", "premiere": "2018-07-28T03:00:00.000Z", "director": "ygerhnwwxp", "viewers": 2},
+        {"actors": ["ywknirn", "gczqzne", "qjmlhysn"], "title": "lzjck3", "premiere": "2018-07-28T03:00:00.000Z", "director": "ygerhnwwxp", "viewers": 3},
+        {"actors": ["ywknirn", "gczqzne", "qjmlhysn"], "title": "lzjck4", "premiere": "2018-07-28T03:00:00.000Z", "director": "ygerhnwwxp", "viewers": 4},
+        {"actors": ["ywknirn", "gczqzne", "qjmlhysn"], "title": "lzjck5", "premiere": "2018-07-28T03:00:00.000Z", "director": "ygerhnwwxp", "viewers": 5},
+        {"actors": ["ywknirn", "gczqzne", "qjmlhysn"], "title": "lzjck6", "premiere": "2018-07-28T03:00:00.000Z", "director": "ygerhnwwxp", "viewers": 6},
+        {"actors": ["ywknirn", "gczqzne", "qjmlhysn"], "title": "lzjck7", "premiere": "2018-07-28T03:00:00.000Z", "director": "ygerhnwwxp", "viewers": 7},
+        {"actors": ["ywknirn", "gczqzne", "qjmlhysn"], "title": "lzjck8", "premiere": "2018-07-28T03:00:00.000Z", "director": "ygerhnwwxp", "viewers": 8},
+        {"actors": ["ywknirn", "gczqzne", "qjmlhysn"], "title": "lzjck9", "premiere": "2018-07-28T03:00:00.000Z", "director": "ygerhnwwxp", "viewers": 9},
+        {"actors": ["ywknirn", "gczqzne", "qjmlhysn"], "title": "lzjck0", "premiere": "2018-07-28T03:00:00.000Z", "director": "ygerhnwwxp", "viewers": 0}
     ]);
+let apiDelete1 = nock(process.env.SERVERIP).persist().delete('/movies/lzjck1').reply(200, {});
+let apiDelete2 = nock(process.env.SERVERIP).persist().delete('/movies/lzjck2').reply(200, {});
+let apiDelete3 = nock(process.env.SERVERIP).persist().delete('/movies/lzjck3').reply(200, {});
+let apiDelete4 = nock(process.env.SERVERIP).persist().delete('/movies/lzjck4').reply(200, {});
+let apiDelete5 = nock(process.env.SERVERIP).persist().delete('/movies/lzjck5').reply(200, {});
+let apiDelete6 = nock(process.env.SERVERIP).persist().delete('/movies/lzjck6').reply(200, {});
+let apiDelete7 = nock(process.env.SERVERIP).persist().delete('/movies/lzjck7').reply(200, {});
+let apiDelete8 = nock(process.env.SERVERIP).persist().delete('/movies/lzjck8').reply(200, {});
+let apiDelete9 = nock(process.env.SERVERIP).persist().delete('/movies/lzjck9').reply(200, {});
+let apiDelete0 = nock(process.env.SERVERIP).persist().delete('/movies/lzjck0').reply(200, {});
 
-let api2 = nock(process.env.SERVERIP)
-    .get('/movies/:title')
-    .reply(200,
-        {
-            "actors": [
-                "ywknirn",
-                "gczqzne",
-                "qjmlhysn"
-            ],
-            "_id": "5b5ceb9a929eb815f6cda063",
-            "title": "lzjck",
-            "premiere": "2018-07-28T03:00:00.000Z",
-            "director": "ygerhnwwxp",
-            "viewers": 0,
-            "__v": 0
-        }
-    );
+let apiPut1 = nock(process.env.SERVERIP).persist().put('/movies/lzjck1').reply(200, {});
+let apiPut2 = nock(process.env.SERVERIP).persist().put('/movies/lzjck2').reply(200, {});
+let apiPut3 = nock(process.env.SERVERIP).persist().put('/movies/lzjck3').reply(200, {});
+let apiPut4 = nock(process.env.SERVERIP).persist().put('/movies/lzjck4').reply(200, {});
+let apiPut5 = nock(process.env.SERVERIP).persist().put('/movies/lzjck5').reply(200, {});
+let apiPut6 = nock(process.env.SERVERIP).persist().put('/movies/lzjck6').reply(200, {});
+let apiPut7 = nock(process.env.SERVERIP).persist().put('/movies/lzjck7').reply(200, {});
+let apiPut8 = nock(process.env.SERVERIP).persist().put('/movies/lzjck8').reply(200, {});
+let apiPut9 = nock(process.env.SERVERIP).persist().put('/movies/lzjck9').reply(200, {});
+let apiPut0 = nock(process.env.SERVERIP).persist().put('/movies/lzjck0').reply(200, {});
 
-let api3 = nock(process.env.SERVERIP)
+
+let apiPost = nock(process.env.SERVERIP)
+    .persist()
     .post('/movies/')
     .reply(201, {});
 
-let api4 = nock(process.env.SERVERIP)
-    .put('/movies/:title')
-    .reply(200, {});
-
-let api5 = nock(process.env.SERVERIP)
-    .delete('/movies/:title')
-
-    .reply(200, {});
-
 const handler = require('../handler.js');
-
 
 describe('Handler tests', () => {
 
-    describe('onStart method', () => {
+    describe('changeMovies method', () => {
 
-        it('Should populate te DB with 10 movies', done => {
-            handler.onStart().should.equal(0);
-
-            done()
+        it('Should change the movies', done => {
+            (async () => {
+                let data = await handler.changeMovies();
+                setTimeout(() => {
+                    let statusGet = apiGet.isDone();
+                    let statusDelete = (apiDelete1.isDone() && apiDelete2.isDone() && apiDelete3.isDone() && apiDelete4.isDone() && apiDelete5.isDone() && apiDelete6.isDone() && apiDelete7.isDone() && apiDelete8.isDone() && apiDelete9.isDone() && apiDelete0.isDone());
+                    let statusPost = apiPost.isDone();
+                    statusGet.should.equal(true);
+                    statusDelete.should.equal(true);
+                    statusPost.should.equal(true);
+                    done();
+                }, 1);
+            })();
         });
     });
 
     describe('increaseViewers method', () => {
 
         it('Should add viewers to a movie', done => {
-            handler.increaseViewers().should.equal(0);
-
-            done()
+            (async () => {
+                let data = await handler.increaseViewers();
+                setTimeout(() => {
+                    let statusGet = apiGet.isDone();
+                    let statusPut = (apiPut1.isDone() || apiPut2.isDone() || apiPut3.isDone() || apiPut4.isDone() || apiPut5.isDone() || apiPut6.isDone() || apiPut7.isDone() || apiPut8.isDone() || apiPut9.isDone() || apiPut0.isDone());
+                    statusGet.should.equal(true);
+                    statusPut.should.equal(true);
+                    done();
+                }, 1);
+            })();
         });
     });
 
-    describe('increaseViewers method', () => {
+    describe('generateStats method', () => {
 
         it('Should return stats', done => {
-            handler.generateStats().should.equal(0);
-
-            done()
-        });
-    });
-
-    describe('cangeMovies method', () => {
-
-        it('Should change the movies', done => {
-            handler.changeMovies().should.equal(0);
-
-            done()
+            (async () => {
+                let data = await handler.generateStats();
+                data.length.should.equal(5);
+                data[0].should.include.keys('title', 'director', 'premiere', 'actors', 'viewers');
+                done();
+            })();
         });
     });
 });
